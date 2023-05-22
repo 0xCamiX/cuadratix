5593160f0888b7acc2d75edf68e7fd1566c57395
CamiloRX777 <juancamilogallego70@gmail.com>
2023-05-22 15:42:42 -0700

    feat: add basic method for rest api

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,46 +1,7 @@
-import Event from "@/models/Event"
-import db from "@/lib/db"
-
-// export async function GET(request: Request) {
-//   await db.connect() // Connect to the database
-
-//   await Event.createCollection()
-
-//   const speaker = {
-//     identification: "123456789",
-//     name: "John Doe",
-//     description: "Lorem impsum dolor sit amet",
-//     photo: "https://picsum.photos/200",
-//     type: "Conferencista",
-//   }
-
-//   const activity = {
-//     name: "actividad test",
-//     type: "Conferencia",
-//     description: "actividad test",
-//     date: new Date(),
-//     endDate: new Date(),
-//     location: "Aula Maxima",
-//     speakers: [speaker],
-//   }
-
-//   const event = {
-//     name: "Evento Test",
-//     location: "UNIAJC SUR",
-//     description: "Evento Test",
-//     date: new Date(),
-//     endDate: new Date(),
-//     activities: [activity],
-//   }
-
-//   await Event.create(event)
-
-//   const events = await Event.find({})
-
-//   return new Response(JSON.stringify(events)) // Return the events as a JSON string
-// }
-
 import { NextRequest, NextResponse } from "next/server"
+import Event from "@/models/Event"
+
+import db from "@/lib/db"
 
 export async function GET() {
   try {
@@ -70,6 +31,50 @@ export async function DELETE(request: NextRequest) {
     await db.disconnect()
 
     return new NextResponse(JSON.stringify(deletedEvent))
+  } catch (error) {
+    console.log(error)
+    return new NextResponse(JSON.stringify({ message: "Error" }))
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const res = await request.json()
+
+  if (!res) return new NextResponse(JSON.stringify({ message: "Invalid data" }))
+
+  try {
+    await db.connect()
+
+    const newEvent = await Event.create(res)
+
+    await db.disconnect()
+
+    return new NextResponse(JSON.stringify(newEvent))
+  } catch (error) {
+    console.log(error)
+    return new NextResponse(JSON.stringify({ message: "Error" }))
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("id")
+
+  if (!id) return new NextResponse(JSON.stringify({ message: "Invalid id" }))
+
+  const res = await request.json()
+
+  if (!res) return new NextResponse(JSON.stringify({ message: "Invalid data" }))
+
+  try {
+    await db.connect()
+
+    const updatedEvent = await Event.findByIdAndUpdate(id, res, {
+      new: true,
+    })
+
+    await db.disconnect()
+
+    return new NextResponse(JSON.stringify(updatedEvent))
   } catch (error) {
     console.log(error)
     return new NextResponse(JSON.stringify({ message: "Error" }))
