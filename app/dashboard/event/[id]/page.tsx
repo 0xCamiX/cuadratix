ddd25ad001efb439cf2dcb22bd4836350ac8252f
CamiloRX777 <juancamilogallego70@gmail.com>
2023-06-01 22:06:37 -0700

    feat: add feature filter speakers for a specific activity

--- app/dashboard/event/[id]/page.tsx
+++ app/dashboard/event/[id]/page.tsx
@@ -1,8 +1,7 @@
-import { format } from "date-fns"
-
 import { IActivity } from "@/models/mongoose/Activity"
 import { IEvent } from "@/models/mongoose/Event"
 import { ISpeaker } from "@/models/mongoose/Speaker"
+import { format } from "date-fns"
 
 import { getAllActivitiesByEvent } from "@/lib/activity"
 import { getOneEventById } from "@/lib/events"
@@ -30,18 +29,18 @@ export default async function EventPage({
         <h3 className="text-xl tracking-tighter">Actividades</h3>
         {/* show activities */}
 
-        <div className="my-4 grid gap-4 sm:grid-cols-2 text-justify">
+        <div className="my-4 grid gap-4 text-justify sm:grid-cols-2">
           {activities.map((activity) => (
             <div className="group relative h-20">
               <li key={activity?._id}>{activity?.name}</li>
               <p className="text-muted-foreground">
                 {`${activity?.description} Fecha Inicio: ${activity?.date} Lugar: ${activity?.location}`}
               </p>
-              {speakers.map((speaker) => (
+              {speakers
+                .filter((speaker) => speaker?.activity === activity?._id) // Filter the speakers based on activity._id
+                .map((speaker) => (
                   <div key={speaker?._id}>
-                    <p className="text-md">
-                      {`Nombre: ${speaker?.name}`}
-                    </p>
+                    <p className="text-md">{`Name: ${speaker?.name}`}</p>
                     <span className="text-muted-foreground">
                       {speaker?.description}
                     </span>
